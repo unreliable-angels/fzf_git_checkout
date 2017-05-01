788af046ad287b2c0f45379f1e2c492a58c4ef30
Hirofumi Wakasugi <baenej@gmail.com>
2017-05-01 11:26:53 +0900

    Warn when not in git repo

--- functions/fzf_git_checkout.fish
+++ functions/fzf_git_checkout.fish
@@ -2,6 +2,8 @@ function fzf_git_checkout -d "fzf source to checkout git branch"
     git rev-parse --is-inside-work-tree >/dev/null ^/dev/null
 
     if not test $status -eq 0
+        command echo 'fzf_git_checkout: Not a git repository'
+        commandline -f execute
         return
     end
 
